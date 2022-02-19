89f49049bf78ff474b19ee8d346562b65a2eb6bb
tannerstuhr <tannerjstuhr@gmail.com>
2022-02-18 17:34:11 -0800

    changed styling of site and aadded JS to make game run in browser instead of prompts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,142 @@
 
+const possibleChoices = ["rock", "paper", "scissors"]
+let botChoice
+let playerChoice
+let botScore = 0
+let playerScore = 0
+
+let buttons = document.querySelectorAll(".button");
+const container = document.querySelector("#results-container");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const img = button.querySelector("img");
+        playerChoice = img.alt.toLowerCase();
+
+        playRound(playerChoice, botChoice)
+
+        if (playerScore === 5 || botScore === 5) {
+            declareWinner();
+        }
+    });
+});
+
+function botPlay() {
+    //let choice = possibleChoices[(Math.floor(Math.random() * possibleChoices.length))];
+    //console.log(choice);
+    //return choice
+    
+    return possibleChoices[~~(Math.random() * possibleChoices.length)];
+}
+
+function playRound(playerChoice, botChoice) {
+    botChoice = botPlay();
+    if(playerChoice == botChoice) {
+        displayResults("It's a tie!");
+    } else if ((botChoice == "rock" && playerChoice == "scissors") ||
+              (botChoice == "paper" && playerChoice == "rock") ||
+              (botChoice == "scissors" && playerChoice == "paper")
+    ) {
+        botScore++
+        keepBotScore();
+        switch(true) {
+            case botScore === 1:
+                displayResults(
+                    `The bot chose ${botChoice} dummy. The bot got a point.`
+                );
+                break;
+            case botScore === 2:
+                displayResults(
+                    `The bot chose ${botChoice} and 
+                    you chose ${playerChoice}... so.. the bot gets another point.`
+                );
+                break;
+            case botScore === 3:
+                displayResults(
+                    `Whelp. The bot chose ${botChoice} and that's definitely going to beat your ${playerChoice}.`
+                );
+                break;
+            case botScore === 4:
+                displayResults(
+                    `Are you even trying? The bot chose ${botChoice} so it's one move from winning.`
+                );
+                break;
+            default:
+                displayResults(`${botChoice} beats ${playerChoice}`);
+                break;
+        }
+    } else {
+        playerScore++
+        keepPlayerScore();
+        switch(true) {
+            case playerScore === 1:
+                displayResults( 
+                    `The bot chose ${botChoice}. Nice job!`
+                );
+                break;
+            case playerScore === 2:
+                displayResults(
+                    `The bot chose ${botChoice}. There you go!`
+                );
+                break;
+            case playerScore === 3:
+                displayResults(
+                    `The bot chose ${botChoice} that time. You're almost there! `
+                );
+                break;
+            case playerScore === 4:
+                displayResults(
+                    `The bot chose ${botChoice}, you only need one more!`
+                );
+                break;
+            default:
+                displayResults(`${playerChoice} beats ${botChoice}`);
+                break;
+    }
+   }
+}
+function displayResults(str) {
+    container.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 300,
+      fill: "forwards",
+      iterations: 1,
+      delay: 0,
+      easing: "ease-out",
+    });
+    container.textContent = str;
+  }
+
+  function keepPlayerScore() {
+      let playerScoreBox = document.querySelector("#player-score");
+
+      playerScoreBox.animate([{ opacity: 0}, { opacity: 1}], {
+          duration: 300,
+          fill: "forwards",
+          iterations: 1,
+          delay: 0,
+          easing: "ease-out",
+         
+      });
+
+      playerScoreBox.textContent = playerScore;
+
+  }
+
+  function keepBotScore() {
+      let botScoreBox = document.querySelector("#bot-score")
+
+      botScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+        duration: 300,
+        fill: "forwards",
+        iterations: 1,
+        delay: 0,
+        easing: "ease-out",
+      });
+    
+      botScoreBox.textContent = botScore;
+  }
+
+/*
 function runGame() {
     let playerScoreDisplay = document.getElementById("player-score");
     let playerScore = 0
@@ -92,4 +230,4 @@ function playRound() {
     return results;
 
 }
-
+*/
